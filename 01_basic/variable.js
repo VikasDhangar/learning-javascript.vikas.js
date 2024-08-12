43af690430e636837147b01d08fd50118147ab17
VikasDhangar <vickydhangar476@gmail.com>
2024-08-12 10:40:57 +0000

    variables-learning

---
+++ 01_basic/variable.js
@@ -0,0 +1,11 @@
+const number = 9999  //constant variable. only one time declar//
+let  myName = "vikas"
+var lastName = "dhangar" //perefer to not say becase it is fuction scope and it is ;
+
+console.log([number,myName,lastName]);
+
+// number = 8989;
+// console.log(number)
+
+myName = "kunal"
+console.log(myName)
